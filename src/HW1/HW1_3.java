0e23be770a3b644e31dcad66f522cf37c951ba3b
cmgsh251831@gmail.com <cmgsh251831@gmail.com>
2024-04-28 22:27:06 +0800

    modified

--- src/HW1/HW1_3.java
+++ src/HW1/HW1_3.java
@@ -2,11 +2,17 @@ package HW1;
 
 public class HW1_3 {
 	public static void main(String[] args) {
+//		請由程式算出256559秒為多少天、多少小時、多少分與多少秒
 		int second = 256559;
-		int minute = second / 60;
-		int hour = second / (60 * 60);
 		int day = second / (60 * 60 * 24);
-		System.out.println("256559秒為" + day + "天、" + hour + "小時、" + minute + "分鐘、" + second + "秒。");
+		int day1 = second % (60 * 60 * 24);
+		int hour = day1 / (60 * 60);
+		int hour1 = day1 % (60 * 60);
+		int minute = hour1 / (60);
+		int second1 = hour1 % (60 );
+
+		System.out.println("256559秒為" + day + "天" + hour + "小時" + minute + "分鐘" + second1 + "秒。");
+		
 	}
 
 }
--- src/HW1/HW1_6.java
+++ src/HW1/HW1_6.java
@@ -3,7 +3,7 @@ package HW1;
 public class HW1_6 {
 	public static void main(String[] args) {
 		System.out.println(5+5); // 5+5為數字相加，結果為10。
-		System.out.println(5+'5'); // '5'為字元字面常數(char)，用單引號表示。字元值定數代表一個Unicode字元，
+		System.out.println(5+'5'); // '5'為字元字面常數(char)，用單引號表示。字元值定數代表一個Unicode字元，其編號以十六進位表示。數字5在unicode邊碼表中為0035，換算
 		System.out.println(5+"5"); // "5"為字串(String)，用雙引號表示。數字+字串會串接，不會相加，故結果為55。
 	}
 
